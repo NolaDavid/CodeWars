e3b1e749ca1de7a6842f0cae38ec8365e80089af
David Guillory <Dguillory62888@gmail.com>
2023-02-10 08:34:06 -0600

    all tests passing on codewars panagram detection

--- 6KYU/DetectPanagram.js
+++ 6KYU/DetectPanagram.js
@@ -3,8 +3,14 @@ A pangram is a sentence that contains every single letter of the alphabet at lea
 
 Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 */
+
+//for this solution we first get all the given letters to be lowercase, we then create a variable to hold the alphabet. we then create a new unique array by converting the string into an array then filtering the new array with the condition of the letter being in the alphabet then we create a new set.
+//we then go through the alphabet and compare the letters at each spot to the unique array built
+
 function isPangram(string) {
   //...
+  string = string.toLowerCase();
+
   const abc = [
     "a",
     "b",
@@ -33,11 +39,22 @@ function isPangram(string) {
     "y",
     "z",
   ];
-  if (string.length < 26) {
-    return false;
-  }
-  let array = string.split("").filter((letter) => abc.indexOf(letter) >= 0);
+
+  let array = string
+    .split("")
+    .filter((letter) => abc.indexOf(letter) >= 0)
+    .sort();
 
   let unique = [...new Set(array)];
-  return unique.length === 26;
+  console.log("unique", unique);
+  if (unique.length < 26) {
+    return false;
+  }
+
+  for (let i = 0; i < abc.length; i++) {
+    if (abc[i] !== unique[i]) {
+      return false;
+    }
+  }
+  return true;
 }
