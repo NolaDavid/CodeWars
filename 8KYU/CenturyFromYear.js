eb510046153ee0c2f07de86e49016945ec7b169e
David Guillory <Dguillory62888@gmail.com>
2023-02-14 10:31:19 -0600

    year to century solution

--- 8KYU/CenturyFromYear.js
+++ 8KYU/CenturyFromYear.js
@@ -3,24 +3,5 @@ The first century spans from the year 1 up to and including the year 100, the se
 */
 
 function century(year) {
-    // Finish this :)
-    let century;
-    year = year.toString();
-   if(year.length < 4){
-     while(year.length < 4){
-       year = '0' + year
-     }
-   }
-    let firstHalf = Number(year.slice(0,2))
-    let secondHalf = Number(year.slice(2))
-   
-    if(secondHalf < 1){
-      
-      century = firstHalf
-    }else {
-     
-      century = firstHalf + 1
-    }
-   
-    return century
+return Math.ceil(year/ 100)
   }
